ad56d7794c53a774a348eb51e8e3dcf448e5e7c2
Sarah George <sarah@atomicrobot.com>
2019-10-02 16:10:08 -0400

    initial commit

---
+++ lib/pages/edit_profile.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class EditProfie extends StatefulWidget {
+  @override
+  _EditProfileState createState() => _EditProfileState();
+}
+
+class _EditProfileState extends State<EditProfie> {
+  @override
+  Widget build(BuildContext context) {
+    return Text('Edit Profile');
+  }
+}
